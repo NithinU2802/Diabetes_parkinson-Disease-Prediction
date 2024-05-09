6d99f986583bb926ab95e677040878168a4ce64e
NithinU2802 <nithinu2810@gmail.com>
2024-05-10 01:16:50 +0530

    RollBack Update

--- index.py
+++ index.py
@@ -4,10 +4,10 @@ import streamlit as st
 
 # loading the saved models
 
-diabetes_model = pickle.load(open('Diabetes_Model.sav', 'rb'))
+diabetes_model = pickle.load(open('Trained_Model/Diabetes_Model.sav', 'rb'))
 
 parkinsons_model = pickle.load(
-    open('Parkinsons_Model.sav', 'rb'))
+    open('Trained_Model/Parkinsons_Model.sav', 'rb'))
 
 
 selected = st.sidebar.selectbox(
