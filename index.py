ab0663a4ff1ccc483dc83162c87a8376896c36bf
NithinU2802 <nithinu2810@gmail.com>
2024-05-18 19:39:44 +0530

    Deployment Update

--- index.py
+++ index.py
@@ -1,165 +1,147 @@
 import pickle
 import streamlit as st
 
+# Function to load the model
+# @st.cache(allow_output_mutation=True)
+def load_model(model_path):
+    with open(model_path, 'rb') as file:
+        model = pickle.load(file)
+    return model
 
-# loading the saved models
+# Load the models
+diabetes_model = load_model('Trained_Model/Diabetes_Model.sav')
+parkinsons_model = load_model('Trained_Model/Parkinsons_Model.sav')
 
-diabetes_model = pickle.load(open('Trained_Model/Diabetes_Model.sav', 'rb'))
-
-parkinsons_model = pickle.load(
-    open('Trained_Model/Parkinsons_Model.sav', 'rb'))
-
-
+# Sidebar for navigation
 selected = st.sidebar.selectbox(
     'Diabetes and Parkinsons Prediction System',
     ['Diabetes Prediction', 'Parkinsons Prediction']
 )
 
+# Title
 st.title("Diabetes and Parkinsons Prediction System")
 
 # Diabetes Prediction Page
-if (selected == 'Diabetes Prediction'):
-
-    # page title
+if selected == 'Diabetes Prediction':
     st.subheader('Diabetes Prediction')
 
-    # getting the input data from the user
+    # Input fields
     col1, col2, col3 = st.columns(3)
 
     with col1:
         Pregnancies = st.text_input('Number of Pregnancies')
-
     with col2:
         Glucose = st.text_input('Glucose Level')
-
     with col3:
         BloodPressure = st.text_input('Blood Pressure value')
-
     with col1:
         SkinThickness = st.text_input('Skin Thickness value')
-
     with col2:
         Insulin = st.text_input('Insulin Level')
-
     with col3:
         BMI = st.text_input('BMI value')
-
     with col1:
-        DiabetesPedigreeFunction = st.text_input(
-            'Diabetes Pedigree Function value')
-
+        DiabetesPedigreeFunction = st.text_input('Diabetes Pedigree Function value')
     with col2:
         Age = st.text_input('Age of the Person')
 
-    # code for Prediction
+    # Code for Prediction
     diab_diagnosis = ''
 
-    # creating a button for Prediction
-
+    # Button for Prediction
     if st.button('Diabetes Test Result'):
-        diab_prediction = diabetes_model.predict(
-            [[Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin, BMI, DiabetesPedigreeFunction, Age]])
+        try:
+            diab_prediction = diabetes_model.predict([[
+                float(Pregnancies), float(Glucose), float(BloodPressure),
+                float(SkinThickness), float(Insulin), float(BMI),
+                float(DiabetesPedigreeFunction), float(Age)
+            ]])
 
-        if (diab_prediction[0] == 1):
-            diab_diagnosis = 'The person is diabetic'
-        else:
-            diab_diagnosis = 'The person is not diabetic'
+            if diab_prediction[0] == 1:
+                diab_diagnosis = 'The person is diabetic'
+            else:
+                diab_diagnosis = 'The person is not diabetic'
+        except ValueError:
+            st.error("Please enter valid input values")
 
     st.success(diab_diagnosis)
 
-
 # Parkinson's Prediction Page
-if (selected == "Parkinsons Prediction"):
-
-    # page title
+if selected == "Parkinsons Prediction":
     st.subheader("Parkinson's Disease Prediction")
 
     col1, col2, col3, col4, col5 = st.columns(5)
 
     with col1:
         fo = st.text_input('MDVP:Fo(Hz)')
-
     with col2:
         fhi = st.text_input('MDVP:Fhi(Hz)')
-
     with col3:
         flo = st.text_input('MDVP:Flo(Hz)')
-
     with col4:
         Jitter_percent = st.text_input('MDVP:Jitter(%)')
-
     with col5:
         Jitter_Abs = st.text_input('MDVP:Jitter(Abs)')
-
     with col1:
         RAP = st.text_input('MDVP:RAP')
-
     with col2:
         PPQ = st.text_input('MDVP:PPQ')
-
     with col3:
         DDP = st.text_input('Jitter:DDP')
-
     with col4:
         Shimmer = st.text_input('MDVP:Shimmer')
-
     with col5:
         Shimmer_dB = st.text_input('MDVP:Shimmer(dB)')
-
     with col1:
         APQ3 = st.text_input('Shimmer:APQ3')
-
     with col2:
         APQ5 = st.text_input('Shimmer:APQ5')
-
     with col3:
         APQ = st.text_input('MDVP:APQ')
-
     with col4:
         DDA = st.text_input('Shimmer:DDA')
-
     with col5:
         NHR = st.text_input('NHR')
-
     with col1:
         HNR = st.text_input('HNR')
-
     with col2:
         RPDE = st.text_input('RPDE')
-
     with col3:
         DFA = st.text_input('DFA')
-
     with col4:
         spread1 = st.text_input('spread1')
-
     with col5:
         spread2 = st.text_input('spread2')
-
     with col1:
         D2 = st.text_input('D2')
-
     with col2:
         PPE = st.text_input('PPE')
 
-    # code for Prediction
+    # Code for Prediction
     parkinsons_diagnosis = ''
 
-    # creating a button for Prediction
+    # Button for Prediction
     if st.button("Parkinson's Test Result"):
-        parkinsons_prediction = parkinsons_model.predict(
-            [[fo, fhi, flo, Jitter_percent, Jitter_Abs, RAP, PPQ, DDP, Shimmer, Shimmer_dB, APQ3, APQ5, APQ, DDA, NHR, HNR, RPDE, DFA, spread1, spread2, D2, PPE]])
+        try:
+            parkinsons_prediction = parkinsons_model.predict([[
+                float(fo), float(fhi), float(flo), float(Jitter_percent),
+                float(Jitter_Abs), float(RAP), float(PPQ), float(DDP),
+                float(Shimmer), float(Shimmer_dB), float(APQ3), float(APQ5),
+                float(APQ), float(DDA), float(NHR), float(HNR), float(RPDE),
+                float(DFA), float(spread1), float(spread2), float(D2), float(PPE)
+            ]])
 
-        if (parkinsons_prediction[0] == 1):
-            parkinsons_diagnosis = "The person has Parkinson's disease"
-        else:
-            parkinsons_diagnosis = "The person does not have Parkinson's disease"
+            if parkinsons_prediction[0] == 1:
+                parkinsons_diagnosis = "The person has Parkinson's disease"
+            else:
+                parkinsons_diagnosis = "The person does not have Parkinson's disease"
+        except ValueError:
+            st.error("Please enter valid input values")
 
     st.success(parkinsons_diagnosis)
 
-
+# Function to set background image
 def set_bg_from_url(url, opacity=1):
-
     footer = """
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
     <footer>
@@ -184,7 +166,7 @@ def set_bg_from_url(url, opacity=1):
             <li>Sharan Shakthi G</li>
         </ul>
     </footer>
-"""
+    """
     st.markdown(footer, unsafe_allow_html=True)
 
     # Set background image using HTML and CSS
@@ -200,7 +182,6 @@ def set_bg_from_url(url, opacity=1):
         """,
         unsafe_allow_html=True
     )
-
 
 # Set background image from URL
 set_bg_from_url(
